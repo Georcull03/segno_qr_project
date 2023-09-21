36362bb12bde1816599d41a9572403ae44770188
George Cullender <cullenge@amazon.com>
2023-09-21 07:50:23 +0000

    Added expand parameter ln 5. This expands the image so that the whole image is kept once rotated.

--- rotated_qrcode.py
+++ rotated_qrcode.py
@@ -2,5 +2,5 @@ import segno
 
 qr_code = segno.make_qr("45 degree turn")
 
-qr_code_rotated = qr_code.to_pil().rotate(45)
+qr_code_rotated = qr_code.to_pil().rotate(45, expand=True)
 qr_code_rotated.save("rotated_qr_code.png")
