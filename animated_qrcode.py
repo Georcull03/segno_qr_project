3546e53823198e6e4c7109b61ac17445a781448e
George Cullender <cullenge@amazon.com>
2023-09-21 08:03:02 +0000

    imported urlopen class which grabs an animated image from the internet.

--- animated_qrcode.py
+++ animated_qrcode.py
@@ -1,3 +1,5 @@
 import segno
+from urllib.request import urlopen
 
 slts_qr_code = segno.make_qr("https://www.youtube.com/watch?v=hTWKbfoikeg")
+nirvana_url = urlopen("https://media.giphy.com/media/LpwBqCorPvZC0/giphy.gif")
