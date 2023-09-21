988be365e02d3f1c7248c95bf7f495d1ac1ee99c
George Cullender <cullenge@amazon.com>
2023-09-21 08:07:03 +0000

    Added to_artistic method to create QR code with image url and a target of where to create the QR code.

--- animated_qrcode.py
+++ animated_qrcode.py
@@ -3,3 +3,8 @@ from urllib.request import urlopen
 
 slts_qr_code = segno.make_qr("https://www.youtube.com/watch?v=hTWKbfoikeg")
 nirvana_url = urlopen("https://media.giphy.com/media/LpwBqCorPvZC0/giphy.gif")
+slts_qr_code.to_artistic(
+    background=nirvana_url,
+    target="animated_qr_code.gif",
+    scale=5,
+)
